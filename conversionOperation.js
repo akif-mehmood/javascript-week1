67bb344c637a878b6493d08069ac051f73f27729
akif-mehmood <muhammadakif148@gmail.com>
2025-01-01 02:29:53 +0500

    all changes

---
+++ conversionOperation.js
@@ -0,0 +1,35 @@
+let score = undefined
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//  "33"     => 33
+//  "33abc"  => NaN
+//  true     => 1
+//  false    => 0
+
+ let isLoggedIn = 0
+// let isLoggedIn = ""
+// let isLoggedIn = "Akif"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// Boolean data type diffrent values
+
+// let isLoggedIn = 1       print => true
+// let isLoggedIn = 0       print => false
+// let isLoggedIn = ""      print => false
+// let isLoggedIn = "Akif"  print => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
