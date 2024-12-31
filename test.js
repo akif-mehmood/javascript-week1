923f076fe052227b2d41e592d364d42891cba6da
akif-mehmood <muhammadakif148@gmail.com>
2025-01-01 01:35:25 +0500

    second day

--- test.js
+++ test.js
@@ -11,6 +11,20 @@ accountCity = "pak"
 
 
 //console.log(accountId);
-console.table([accountId, accountEmail, accountPassword, accountCity]);
+//console.table([accountId, accountEmail, accountPassword, accountCity]);
 
+//   DATA TYPES
 
+// number    => 2 to power 53
+// bigint    => large value add
+// string    => " " / ' '
+// boolean   => true / false 
+// null      => standalone value / empty not zero 0 
+// undefined 
+// symbol    => unique 
+
+// object 
+
+console.log(typeof undefined); // undefined
+
+console.log(typeof undefined); // object
