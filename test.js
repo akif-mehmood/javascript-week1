7edbde7fa3dcc4b688222fbec553768d384462b0
akif-mehmood <muhammadakif148@gmail.com>
2024-12-31 13:50:49 +0500

    Commit changed

---
+++ test.js
@@ -0,0 +1,16 @@
+const accountId = 1224
+let accountEmail = "akif@gmail.com"
+var accountPassword = "1234"
+accountCity = "Karachi"
+
+
+
+accountEmail = "mehmood@gmail.com"
+accountPassword = "786"
+accountCity = "pak"
+
+
+//console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+
