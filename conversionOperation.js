b4bbaaa4cc0c61cc90cfeccb4c2c6a193ca95aea
akif-mehmood <muhammadakif148@gmail.com>
2025-01-02 02:56:28 +0500

    new update

--- conversionOperation.js
+++ conversionOperation.js
@@ -38,7 +38,7 @@ let stringNumber = String(someNumber)
 
 let value = 3
 let negvalue = -value
-console.log(negvalue);
+// console.log(negvalue);
 
 // console.log(2+2);
 // console.log(2-2);
@@ -51,6 +51,32 @@ let str1 = "akif"
 let str2 = "mehmood"
 
 let str3 = str1 + str2
-console.log(str3);
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(3 + 4 * 5 % 3);
+
+// console.log((2 + 3) * 5 % 3);
+
+//console.log(+true);
+//console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
 
 
