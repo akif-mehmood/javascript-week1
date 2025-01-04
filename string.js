ba6813076ac8a6c808bd2cf6453167f0d96d940a
akif-mehmood <muhammadakif148@gmail.com>
2025-01-04 06:04:49 +0500

    string methods

--- string.js
+++ string.js
@@ -9,17 +9,39 @@ const age = 25
 
 // output => Hello my name is akif-mehmood and my age is 25 ok.
 
-const game = new String('counter-strike');
+//const game = new String('counter-strike');
 
-// check or eccess key value 
-console.log(funtime[0]);  //  output => c  (string first word print)
+// check or eccess key value
+
+//console.log(funtime[0]);  //  output => c  (string first word print)
 
 // check Method
 
 const funtime = new String('counter-strike');
 
 console.log(funtime.__proto__);  //  output => {}
-console.log(funtime.legth);
+
+console.log(funtime.length);  //  output =>  14
+
+console.log(funtime.toUpperCase());  //  output => COUNTERSTRIKE
+
+console.log(funtime.charAt(4));  //  output => t / becouse start counting 0 then 1 so answer "t"
+
+console.log(funtime.indexOf('r'));  //  output => 6 / becouse start counting 0 then 1 so answer "6"
+
+const newString = funtime.substring(0, 4) 
+
+console.log(newString);  //  output => coun  / start 0 1 2 3 but 4 no print 
+
+const anotherString = funtime.slice(-14, 5) 
+
+console.log(anotherString);
+
+
+
+
+
+
 
 
 
