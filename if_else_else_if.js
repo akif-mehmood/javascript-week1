22d05383cffdb4b483ac0fc6a0136a0925b9d0be
akif-mehmood <muhammadakif148@gmail.com>
2025-07-04 18:12:51 +0500

    early return pattren

--- if_else_else_if.js
+++ if_else_else_if.js
@@ -34,3 +34,24 @@ function getGrade(score) {
     }   
 }
 console.log(getGrade(9));
+
+// short methord  // early return pattern
+
+function getGrade(score) {
+    if (score >= 90 && score <= 100) return "A+";
+        
+         if (score >= 80 && score <= 89) return "B";
+            
+         if (score >= 70 && score <= 79) return "C";
+        
+         if (score >= 60 && score <= 69) return "D";
+        
+         if (score >= 33 && score <= 59) return "E";
+        
+         if (score >= 0 && score <= 32)  return "Fail";
+
+        return "Invalid Mark";
+}
+console.log(getGrade(95));
+
+ 
